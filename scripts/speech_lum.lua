16806c43fcebab83ac45edfe5573e32bf6c67836
wisard1862 <danieswain@gmail.com>
2014-05-27 12:41:10 -0400

    Speech Update
    Speech scripts added.

--- scripts/speech_lum.lua
+++ scripts/speech_lum.lua
@@ -1,8 +1,66 @@
 return {
+	ACTIONFAIL = 
+	{
+		SHAVE =
+		{
+			AWAKEBEEFALO = "'mere you.",
+			GENERIC = "I can't do anymore to that.",
+			NOBITS = "Mmm. Sexy.",
+		},
+
+		STORE =
+		{
+			GENERIC = "I'm stuffed!",
+			NOTALLOWED = "Square peg in a round hole.",
+		},
+	},
 
 	ACTIONFAIL_GENERIC = "Maybe next time, champ?",
+	ANNOUNCE_ACCOMPLISHMENT = "I know. I'm good.",
+	ANNOUNCE_ACCOMPLISHMENT_DONE = "Come at me.",
 	ANNOUNCE_ADVENTUREFAIL = "Better luck next time.",
+	ANNOUNCE_BEES = "Buzz, buzz!",
 	ANNOUNCE_BOOMERANG = "Fly, my friend...",
+	ANNOUNCE_CANFIX = "We can rebuild it. We have the technology",
+	ANNOUNCE_CHARLIE = "Huh.",
+	ANNOUNCE_CHARLIE_ATTACK = "Who?!",
+	ANNOUNCE_COLD = "This fur is only for show, you know.",
+	ANNOUNCE_CRAFTING_FAIL = "Need... More...",
+	ANNOUNCE_DEERCLOPS = "That's a big boy.",
+	ANNOUNCE_DUSK = "The speed...",
+	ANNOUNCE_EAT =
+	{
+		GENERIC = "Not half bad.",
+		PAINFUL = "Okay, that's bad.",
+		SPOILED = "Tastes like mold.",
+		STALE = "Needs ketchup.",
+	},
+
+	ANNOUNCE_ENTER_DARK = "This is really difficult with one eye.",
+	ANNOUNCE_ENTER_LIGHT = "Much better.",
+	ANNOUNCE_FREEDOM = "Goodie!",
+	ANNOUNCE_HIGHRESEARCH = "Did I tell you I have a Bachelors in BS?",
+	ANNOUNCE_HOUNDS = "Oh no, I think I'm having flashbacks...",
+	ANNOUNCE_HUNGRY = "Feeeeed me.",
+	ANNOUNCE_HUNT_BEAST_NEARBY = "Nose sharpens... Ears perked.",
+	ANNOUNCE_HUNT_LOST_TRAIL = "Sniff, sniff... Oh, wait. This is 'my' scent.",
+	ANNOUNCE_INV_FULL ="We're gonna need a bigger backpack.",
+	ANNOUNCE_KNOCKEDOUT = "Ooof.",
+	ANNOUNCE_LOWRESEARCH = "I can make axes and dogs can't look up.",
+	ANNOUNCE_MOSQUITOS = "You suck.",
+	ANNOUNCE_NODANGERSLEEP = "No sleep for the wicked.",
+	ANNOUNCE_NODAYSLEEP = "Well at least only one of my eyes is baggy.",
+	ANNOUNCE_NODAYSLEEP_CAVE = "Oof. I'm beat.",
+	ANNOUNCE_NOHUNGERSLEEP = "I think I almost chewed my tongue off.",
+	ANNOUNCE_NO_TRAP = "Well.",
+	ANNOUNCE_PECKED = "No, not my good eye!",
+	ANNOUNCE_QUAKE = "S-Shaaakey-y!",
+	ANNOUNCE_RESEARCH = "Head... Hurts.",
+	ANNOUNCE_THORNS = "Ack! I'm holy enough.",
+	ANNOUNCE_TORCH_OUT = "Uhm...",
+	ANNOUNCE_TRAP_WENT_OFF = "Traps-ahoy!",
+	ANNOUNCE_UNIMPLENTED = "Not yet.",
+	ANNOUNCE_WORMHOLE = "How much did I drink?",
 	BATTLECRY =
 	{
 		GENERIC = "Hey, stay right there!",
@@ -11,62 +69,148 @@ return {
 		SPIDER = "I'll have an eye, thanks.",
 		SPIDER_WARRIOR = "Eye for an eye, haha.",
 	},
+
+	COMBAT_QUIT =
+	{
+		GENERIC = "I didn't want to kill you anyway.",
+		PIG = "Pigmen are so overrated.",
+		PREY = "Baby... come back.",
+		SPIDER = "I didn't want to kill you anyway.",
+		SPIDER_WARRIOR = "I didn't want to kill you anyway.",
+	},
+
 	DESCRIBE =
 	{
+		ARMORSLURPER = "Hmm, what a suit.",
+		ARMORRUINS = "If only it were whole...",
 		AXE = "Let me 'AXE' you a question.. HA!",
 		BAT = "It doesn't even use it's eyes.",
 		BERRIES = "Ripe and ready.",
-		-- BIRD = "",
+		BIRD = "Flappy.",
+		BISHOP_NIGHTMARE = "Ah! It moves...",
 		BOOMERANG = "I've missed you, pal.",
-		-- BERRYBUSH = "",
+		BERRYBUSH = "Oh yum,",
 		BUTTERFLY = "Go back from whence you came, foul creature!",
-		CARROT = "",
+		CARROT = "Good for my eye.",
 		CAVE_BANANA = "Oh, how I'd like one right now",
+		EEL = "Looks like a big worm.",
+		EEL_COOKED = "Cooked sea worm.",
 		EVERGREEN = "You're just so sure, aren't you.",
+		EYETURRET = "Now if I could just take it...",
+		EYETURRET_ITEM = "How do you seperate them?!",
 		FLINT = "So sharp. Wow.",
 		FLOWER = "Smells like spring.",
+		GEMSOCKET = 
+		{
+			VALID = "Working as intended!",
+			GEMS = "It needs additional pyl-ahem, I mean a gem.",
+		},
+
 		GRASS = 
 		{
 				GENERIC = "A necessary part for a balanced breakfast.",	
 				PICKED = "Give it some time.",
 		},
+
+		GREENGEM = "Such green!",
+		GREENSTAFF = "Pew!",
+		KNIGHT_NIGHTMARE = "Ah! It moves...",
+		MINOTAUR = "Oh ho ho, big boy!",
+		MINOTAURHORN = "This thing can take an eye out!",
+		MINOTAURCHEST = "Hairy.",
+		MULTITOOL_AXE_PICKAXE = "Aha! Now THIS can take an eye out!",
+		NIGHTMARELIGHT = "Hmm. Interesting.",
+		NIGHTMARE_TIMEPIECE = 
+		{
+			CALM = "Nothing bad happening. That's good.",
+			DAWN = "The sun is here!",
+			NOMAGIC = "Uhhh, what happened to the magic?",
+			STEADY = "Hold...",
+			WARN = "I'm sensing something magical.",
+			WANING = "Weakening!",
+			WAXING = "Strengthing!",
+		},
+
+		ORANGEAMULET = "This goes so well with my eye!",
+		ORANGEGEM = "I just want to pop this bad boy in my socket.",
+		ORANGESTAFF = "Pew! Pew!",
 		PIGGUARD = "I guess they allow anyone to be a guard these days.",
+		POND_ALGAE = "I ain't eatin' this.",
 		POOP = "Dookie butter",
+		POTTEDFERN = "Ahh, to be in a cage.",
 		RABBIT =
 		{
 				GENERIC = "Ahh, nature's lean cuisine.",
 				HELD = "It'll look even better cooked",
 		},
+
 		RABBITHOLE = "I wonder if I were to...",
+		RELIC = "This belongs in an antique store!",
 		ROCK = "I'm not THAT hungry, now.",
 		ROCKS = "I wonder if they multiply when I'm not around.",
+		ROOK_NIGHTMARE = "Ah! It moves...",
+		RUINS_BAT = "Big boy.",
+		RUINS_BUBBLE = "A little broken.",
+		RUINSHAT = "Call me Indy.",
 		SAPLING = 
 		{
 			BURNING = "Ah, geeze.",
 			GENERIC = "Hey there, little guy!",
 			PICKED = "I'll be back.",
 		},
+
 		SEEDS = "Hello, little one.",
 		SKELETON = "Poor slob.",
+		SLURPER = "So cute, yet... frightening.",
+		SLURPERPELT = "If only I'd taken a class on sewing.",
+		SLURPER_PELT = "If only I'd taken a class on sewing.",
 		SMALLBIRD =
 		{
 			GENERIC = "Hail, Avian.",
 			HUNGRY = "I wonder if it's got influenza?",
 			STARVING = "I didn't know my stomach made bird calls.",
 		},
+
 		SPIDER =
 		{
 			GENERIC = "I'm jelly, I'll admit it.",
 			DEAD = "Not so bright eyed now, are ya?",
 			SLEEPING = "Sleep... For now...",
 		},
+
+		SPIDER_DROPPER = "Oh no...",
+		STAFFLIGHT = "Lumoz!",
+		TELEBASE =
+		{
+			VALID = "Please work... please work...",
+			GEMS = "It needs additional pyl-ahem, I mean gems.",
+		},
+
 		TENTACLE = "Haha, not in your dreams.",
+		THULECITE = "Thulecite... I know this color.",
+		THULECITE_PIECES = "Thulecite chunks.",
 		TWIGS = "Sticks and stones",
+		UNAGI = "Oooh, smells nice.",
+		WORM = 
+		{
+			PLANT = "Planty!",
+			DIRT = "Do I look like a peasant?",
+			WORM = "Where's your apple, little fella?",
+
+		},
+
+		WORMLIGHT = "Ahh, light.",
 		WORMHOLE =
 		{
 			GENERIC = "This ain't right. IT AIN'T RIGHT!",
 			OPEN = "It almost looks cute. Almost.",
 		},
-			
+
+		YELLOWAMULET = "Mmm, warm.",
+		YELLOWGEM = "It has a yellow-ish, gem-ish look.",
+		YELLOWSTAFF = "Pew, pew. Pew!",
 	},
+	
+	DESCRIBE_GENERIC = "What a thing.",
+	DESCRIBE_TOODARK = "How am I supposed to describe it... It's too dark.",
 }
